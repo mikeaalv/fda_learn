0b92b81dcedc97b3cc4a85650e472b1cc09ed2a8
mikeaalv <yue.wu@uga.edu>
2020-05-05 14:57:07 -0400

    Update fda_pca.m
    caution on running fda pca on multi-dimension data(multiple features)
    use proportion in variance to plot instead of eigen value for easy intepretation

--- fda_pca.m
+++ fda_pca.m
@@ -6,12 +6,13 @@ function [returnstr]=fda_pca(linefda,pcfda,npc,colvec)
 % The user can use the following to plot the PC curve
 %%              subplot(1,1,1)
 %%              plot_pca_fd(fdarotpcastr)
-%% Be careful when Ymat is multi-dimension
-% Argument:
-%         linefda: struct. The fda structure for original smoothed line. Must be provided.
+%% Be careful when Ymat is multi-dimension, in which case the score plot is just the preojecting of first feature(dimension) on PC1 and PC2.
+%
+% Argument:         linefda: struct. The fda structure for original smoothed line. Must be provided.
 %         pcfda: struct. The fda structure for PCs. Must be provided. lambda is set here and the user might try multiple choices.
-%         npc: numeric. the number of pc to get. default 2.
+%         npc: numeric. the number of pc to get/retrieve. default 2.
 %         colvec: vector. the color vector for score plot. optional
+%
 % Return: return the rotated functional pca result.
 % CITATION:
 %         Ramsay, James & Hooker, Giles & Graves, Spencer. (2009). Functional data analysis with R and MATLAB. 10.1007/978-0-387-98185-7.
@@ -20,6 +21,8 @@ function [returnstr]=fda_pca(linefda,pcfda,npc,colvec)
 %     https://cran.r-project.org/web/packages/fda/index.html
 %     http://www.psych.mcgill.ca/misc/fda/downloads/FDAfuns/
 %
+% CAUTION:
+% the function seems to have bugs for multi-dim FDA PCA.
 % YUE WU 01012020
 
 
@@ -45,9 +48,10 @@ fdarotpcastr=varmx_pca(fdapcastr);
 % subplot(1,1,1)
 % plot_pca_fd(fdarotpcastr)
 %  plot log eigenvalues
-fdaharmeigval=fdapcastr.values;
+fdaharmeigval=fdapcastr.varprop;
+nshow=npc;
 h1=figure();
-  plot(1:19,fdaharmeigval(1:19),'-o');
+  plot(1:nshow,fdaharmeigval(1:nshow),'-o');
   xlabel('Eigenvalue Number');
   ylabel('Eigenvalue');
 %  plot factor scores
